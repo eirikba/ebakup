51daf5c294be667d743a5f9d3070c862f5b32059
Eirik Byrkjeflot Anonsen <eirik@eirikba.org>
2015-06-04 13:59:49 +0200

    fake_filesys: If mtime is provided for a file, default mtime_ns to 0

--- tests/fake_filesys.py
+++ tests/fake_filesys.py
@@ -215,6 +215,8 @@ class FakeFileSystem(object):
             if mtime_ns is not None:
                 assert mtime.microsecond == mtime_ns // 1000
             item.mtime = mtime
+            if mtime_ns is None:
+                mtime_ns = 0
         if mtime_ns is not None:
             item.mtime_ns = mtime_ns
         self._paths[path] = item
