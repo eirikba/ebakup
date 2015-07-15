0bc8ad570288e94ee4c81c7c0aa4dc443833af3b
Eirik Byrkjeflot Anonsen <eirik@eirikba.org>
2015-07-15 16:54:48 +0200

    Implement infrastructure for web server

---
+++ pyebakup/http_server.py
@@ -0,0 +1,469 @@
+#!/usr/bin/env python3
+
+import errno
+import re
+import select
+import socket
+
+class ParseFailedError(Exception): pass
+
+class NullHandler(object):
+    '''Implements the request handler interface with empty methods.
+
+    A single handler instance will receive exactly one call to each of
+    these methods, in order:
+    - handle_request_received()
+    - handle_headers_complete()
+    - handle_request_complete()
+    - handle_waiting_for_response()
+
+    In addition, handle_response_ready() will be called exactly once,
+    at any time before handle_waiting_for_response().
+
+    There is one exception: handle_request_aborted() may be called at
+    any time. If that happens, no further methods will be called on
+    that object.
+    '''
+    def handle_raw_data_received(self, data):
+        '''Provides the raw data of the request.
+
+        This method is called after the methods providing the parsed
+        data, such as handle_request_received() and
+        handle_header_received().
+
+        But it is called after methods marking state changes, such as
+        handle_headers_complete() and handle_request_complete().
+        '''
+
+    def handle_request_received(self, method, resource):
+        '''Called once at the start of a request.
+
+        This method is called when the request line (the first line of
+        the full request) is parsed. 'method' is the method of the
+        request (e.g. b'GET') and 'request' is the resource requested
+        (e.g. b'/').
+        '''
+
+    def handle_header_received(self, header, value):
+        '''Called whenever a new header is received.
+
+        This will only be called after handle_request_received() and
+        before handle_headers_complete().
+        '''
+
+    def handle_headers_complete(self):
+        '''Called once when all the request's headers have been received.
+        '''
+
+    def handle_body_data(self, data):
+        '''Called whenever more data is received for the body of the request.
+
+        This will only be called after handle_headers_complete() and
+        before handle_request_complete().
+        '''
+
+    def handle_request_complete(self):
+        '''Called once when the whole request has been received.
+        '''
+
+    def handle_response_ready(self, response):
+        '''Called once when the connection is ready to send the response.
+
+        This may happen much later than handle_request_complete() if
+        the connection is pipelining requests and it takes a long time
+        to send an earlier response. Conversely it may happen long
+        before handle_request_complete(), e.g. if this is the first
+        request on a connection and it takes a long time to receive
+        the complete request.
+
+        The 'response' object provides the methods to construct the
+        response. It remains valid to use until the response is
+        completed or handle_request_aborted() is called.
+
+        The 'response' object provides at least:
+        - send_response(response, content_type)
+        - send_header(header, value)
+        - send_body_size(size)
+        - send_headers_done()
+        - send_body_data(data)
+        - send_response_done()
+
+        send_response(), send_headers_done() and send_response_done()
+        MUST be called once each, in the given order. send_header()
+        and send_body_size() can only be called between
+        send_response() and send_headers_done(). send_body_data() can
+        only be called between send_headers_done() and
+        send_response_done().
+
+        Each of the send_* methods sends some data to the client, but
+        the data may be buffered rather than sent immediately. It is
+        guaranteed that send_headers_done() will cause all the headers
+        to be sent and send_response_done() will cause the whole
+        response to be sent.
+
+        The 'response' object provides some methods to manage the
+        actual sending:
+        - send_buffered_data()
+
+        send_buffered_data() makes sure that all the data that has
+        been buffered so far will be sent to the client. This is
+        essentially the same thing that send_headers_done() and
+        send_response_done() does to ensure that the headers and
+        response is sent.
+        '''
+
+    def handle_waiting_for_response(self):
+        '''Called once when the whole request has been received and the
+        connection is ready to send the response.
+
+        This method will be called immediately after
+        handle_request_complete() or handle_response_ready(),
+        whichever is called last.
+        '''
+
+    def handle_request_aborted(self):
+        '''Called if the request was aborted.
+
+        Once this is called, no more methods will be called on this
+        object. And there is no need to do anything more about the
+        response.
+        '''
+
+    def handle_data_sent(self, pending_size):
+        '''Called whenever some of the response data has been successfully
+        sent to the network.
+
+        'pending_size' is the number of octets of response data still
+        waiting to be sent.
+        '''
+
+class HttpServer(object):
+    def __init__(self, handler, port=None, port_low=None, port_high=None):
+        if port is not None:
+            assert port_low is None
+            assert port_high is None
+            self._port_low = port
+            self._port_high = port
+        else:
+            assert port_low is not None
+            assert port_high is not None
+            self._port_low = port_low
+            self._port_high = port_high
+        self._handler = handler
+        self.port = None
+
+    def stop(self):
+        self.running = False
+
+    def serve_forever(self):
+        self.running = True
+        self._sock_listen = socket.socket()
+        self._sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        bound = False
+        port = self._port_low
+        while not bound:
+            try:
+                self._sock_listen.bind(('127.0.0.1', port))
+                bound = True
+            except OSError as e:
+                if e.errno != errno.EADDRINUSE:
+                    raise
+                port += 1
+                if port > self._port_high:
+                    raise
+        self._sock_listen.settimeout(0)
+        self._sock_listen.listen(10)
+        self.port = port
+        self._sockets = {}
+        self._poller = select.poll()
+        self._poller.register(self._sock_listen)
+        while self.running:
+            for pollfd in self._poller.poll():
+                if pollfd[0] == self._sock_listen.fileno():
+                    self._handle_connect()
+                else:
+                    self._handle_communicate(self._sockets[pollfd[0]])
+
+    def _handle_connect(self):
+        try:
+            sock, remote_addr = self._sock_listen.accept()
+        except BlockingIOError:
+            return
+        sock.settimeout(0)
+        self._poller.register(sock)
+        self._sockets[sock.fileno()] = SocketData(
+            socket=sock, server=self, handler=self._handler)
+
+    def _handle_communicate(self, socket):
+        socket.write_pending_data()
+        socket.read_data()
+
+    def register_for_write(self, socket):
+        self._poller.modify(socket.fileno, select.POLLIN | select.POLLOUT)
+
+    def unregister_for_write(self, socket):
+        self._poller.modify(socket.fileno, select.POLLIN)
+
+    def handle_socket_closed(self, socket):
+        self._poller.unregister(socket.fileno)
+        del self._sockets[socket.fileno]
+
+
+class SocketData(object):
+    def __init__(self, socket, server, handler):
+        self.socket = socket
+        self.server = server
+        self.fileno = socket.fileno()
+        self.pending_data = b''
+        self.handler = handler
+        self.requests = []
+
+    def __str__(self):
+        return '<SocketData ' + str(self.socket) + '>'
+
+    def write_data(self, data):
+        #print('write', self.socket, data)
+        had_pending_data = self.has_pending_data
+        self.pending_data += data
+        self.write_pending_data()
+        if not had_pending_data and self.has_pending_data:
+            self.server.register_for_write(self)
+
+    @property
+    def has_pending_data(self):
+        return bool(self.pending_data)
+
+    def write_pending_data(self):
+        if self.pending_data:
+            try:
+                wrote = self.socket.send(self.pending_data)
+            except BlockingIOError:
+                return
+            except BrokenPipeError:
+                self._handle_socket_closed()
+                return
+            self.pending_data = self.pending_data[wrote:]
+            if not self.pending_data:
+                self.server.unregister_for_write(self)
+
+    def read_data(self):
+        try:
+            data = self.socket.recv(4096 * 64)
+        except BlockingIOError:
+            return False
+        if data == b'':
+            self._handle_socket_closed()
+            self.socket.close()
+            self.server.handle_socket_closed(self)
+        else:
+            self._handle_new_data(data)
+        return True
+
+    def _handle_socket_closed(self):
+        requests = self.requests
+        self.requests = None
+        for req in requests:
+            req.handle_socket_closed()
+
+    def _handle_new_data(self, data):
+        while data:
+            if not self.requests:
+                req = HttpHandler(self, self.handler())
+                req.handle_response_ready()
+                self.requests.append(req)
+            req = self.requests[-1]
+            data = req.handle_new_data(data)
+            if data and self.requests:
+                self.requests.append(HttpHandler(self, self.handler()))
+
+    def handle_response_complete(self, handler):
+        assert handler == self.requests[0]
+        self.requests = self.requests[1:]
+        if self.requests:
+            self.requests[0].handle_response_ready()
+
+class HttpHandler(object):
+    def __init__(self, socket, handler):
+        self.socket = socket
+        self.handler = handler
+        self.received_data = b''
+        self.state = 0 # 0:Start, 1:Headers, 2:Body, 3:End
+        self.response = None
+
+    def write_data(self, data):
+        self.socket.write_data(data)
+
+    def handle_response_complete(self, response):
+        assert response == self.response
+        self.socket.handle_response_complete(self)
+
+    def handle_socket_closed(self):
+        self.handler.handle_request_aborted()
+
+    def handle_new_data(self, data):
+        data = self.received_data + data
+        self.received_data = b''
+        start = 0
+        done = start
+        if self.state == 0:
+            done = self._parse_request_line(data, done)
+        prev_state = self.state
+        prev = done - 1
+        while self.state == 1 and prev != done:
+            prev = done
+            done = self._parse_header_line(data, done)
+        if self.state == 2 and start < done:
+            self.handler.handle_raw_data_received(data[start:done])
+            start = done
+        if prev_state < 2 and self.state >= 2:
+            self.handler.handle_headers_complete()
+        prev_state = self.state
+        if self.state == 2:
+            done = self._parse_body_data(data, done)
+        if done > start:
+            self.handler.handle_raw_data_received(data[start:done])
+        if prev_state < 3 and self.state >= 3:
+            self.handler.handle_request_complete()
+            if self.response:
+                self.handler.handle_waiting_for_response()
+        if self.state >= 3:
+            return data[done:]
+        self.received_data = data[done:]
+        return None
+
+    def handle_response_ready(self):
+        if self.response:
+            return
+        self.response = HttpResponse(self)
+        self.handler.handle_response_ready(self.response)
+        if self.state >= 3:
+            self.handler.handle_waiting_for_response()
+
+    re_request_line = re.compile(
+        br'([a-zA-Z0-9]+) (.*) HTTP/([0-9]+)\.([0-9]+)\r?\n')
+    def _parse_request_line(self, data, done):
+        while data.startswith(b'\r\n', done):
+            done += 2
+        while data.startswith(b'\n', done):
+            done += 1
+        end = data.find(b'\n', done)
+        if end < 0:
+            return done
+        match = self.re_request_line.match(data, done, end+1)
+        if not match:
+            raise ParseFailedError(
+                'Failed to parse request line: ' + repr(data[done:end+1]))
+        method = match.group(1)
+        resource = match.group(2)
+        http_maj = match.group(3)
+        http_min = match.group(4)
+        if http_maj != b'1' or http_min not in (b'0', b'1'):
+            raise ParseFailedError(
+                'Unknown HTTP version: ' + str(http_maj) + '.' + str(http_min))
+        self.state = 1
+        self.handler.handle_request_received(method, resource)
+        return match.end()
+
+    re_header_line = re.compile(br'([-a-zA-Z0-9]+): *([^\r\n]*)\r?\n')
+    def _parse_header_line(self, data, done):
+        if data.startswith(b'\r\n', done):
+            self.state = 2
+            return done + 2
+        elif data.startswith(b'\n', done):
+            self.state = 2
+            return done + 1
+        match = self.re_header_line.match(data, done)
+        if not match:
+            if len(data) < done + 2:
+                return done
+            line_end = data.find(b'\n', done)
+            if line_end < 0:
+                return done
+            raise ParseFailedError(
+                'Failed to parse header line: ' + repr(data[done:line_end+1]))
+        header = match.group(1)
+        value = match.group(2)
+        self._handle_header(header, value)
+        self.handler.handle_header_received(header, value)
+        return match.end()
+
+    def _handle_header(self, header, value):
+        pass
+
+    def _parse_body_data(self, data, done):
+        self.state = 3
+        return done
+
+
+class HttpResponse(object):
+    def __init__(self, http):
+        self._http = http
+        self._responsedata = []
+        self._state = 0 # 0: start, 1: headers, 2: body, 3: done
+        self._close_connection = False
+        self._response_body_size = None
+        self._response_body_sent = 0
+        self._te_chunked = False
+
+    def send_response(self, response, contenttype=None):
+        assert self._state == 0
+        assert not self._responsedata
+        self._responsedata.append(b'HTTP/1.1 ' + response + b'\r\n')
+        self._state = 1
+        if contenttype is not None:
+            self.send_header(b'Content-Type', contenttype)
+
+    def send_header(self, header, value):
+        assert self._state == 1
+        self._responsedata.append(header + b': ' + value + b'\r\n')
+
+    def set_response_body_size(self, size):
+        assert self._state == 1
+        self._response_body_size = size
+
+    def send_headers_done(self):
+        assert self._state == 1
+        if self._response_body_size is not None:
+            self.send_header(
+                b'Content-Size', str(self._response_body_size).encode('utf-8'))
+        else:
+            self._te_chunked = True
+            self.send_header(b'Transfer-Encoding', b'chunked')
+        if self._close_connection:
+            self.send_header(b'Connection', b'close')
+        self._responsedata.append(b'\r\n')
+        self.send_buffered_data()
+        self._state = 2
+
+    def send_buffered_data(self):
+        if not self._responsedata:
+            return
+        chunked = self._state >= 2 and self._te_chunked
+        if chunked:
+            self._responsedata.append(b'\r\n')
+        data = b''.join(self._responsedata)
+        self._responsedata = []
+        if chunked:
+            self._http.write_data(
+                hex(len(data)-2)[2:].encode('utf-8') +
+                b'\r\n')
+        self._http.write_data(data)
+
+    def send_body_data(self, data):
+        assert self._state == 2
+        self._response_body_sent += len(data)
+        if self._response_body_size is not None:
+            assert self._response_body_sent <= self._response_body_size
+        self._responsedata.append(data)
+
+    def send_response_done(self):
+        assert self._state == 2
+        self._state = 3
+        self.send_buffered_data()
+        if self._te_chunked:
+            self._http.write_data(b'0\r\n\r\n')
+        else:
+            assert self._response_body_sent == self._response_body_size
+        if self._close_connection:
+            self._http.close_connection()
+        self._http.handle_response_complete(self)
---
+++ tests/http_server_tests.py
@@ -0,0 +1,239 @@
+#!/usr/bin/env python3
+
+import socket
+import threading
+import time
+import unittest
+
+import http_server
+
+class Handler(object):
+    def __init__(self):
+        self.raw_data = b''
+        self.method = None
+        self.resource = None
+        self.headers = {}
+        self.body = b''
+        self.headers_complete = False
+        self.request_complete = False
+        self.response = None
+
+    def handle_raw_data_received(self, data):
+        self.raw_data += data
+
+    def handle_request_received(self, method, resource):
+        assert self.method is None
+        self.method = method
+        self.resource = resource
+        assert self.method is not None
+
+    def handle_header_received(self, header, value):
+        assert self.method is not None
+        assert not self.headers_complete
+        if header not in self.headers:
+            self.headers[header] = value
+            return
+        oldvalue = self.headers[header]
+        if isinstance(oldvalue, list):
+            oldvalue.append(value)
+            return
+        self.headers[header] = [oldvalue, value]
+
+    def handle_headers_complete(self):
+        assert self.method is not None
+        assert not self.headers_complete
+        self.headers_complete = True
+
+    def handle_body_data(self, data):
+        assert self.headers_complete
+        assert not self.request_complete
+        self.body += data
+
+    def handle_request_complete(self):
+        assert self.headers_complete
+        assert not self.request_complete
+        self.request_complete = True
+
+    def handle_response_ready(self, response):
+        self.response = response
+
+    def handle_waiting_for_response(self):
+        assert self.response
+        assert self.request_complete
+        if self.resource == b'/simple':
+            self._send_simple_resource()
+            return
+        if self.resource.startswith(b'/echo/'):
+            self._send_echo_resource()
+            return
+        self._send_404()
+
+    def _send_simple_resource(self):
+        self.response.send_response(b'200 OK', b'text/plain')
+        self.response.send_headers_done()
+        self.response.send_body_data(b'simple resource')
+        self.response.send_response_done()
+
+    def _send_echo_resource(self):
+        self.response.send_response(b'200 OK', b'text/plain')
+        self.response.send_headers_done()
+        self.response.send_body_data(b'echo: ')
+        self.response.send_body_data(self.resource[6:])
+        self.response.send_response_done()
+
+    def _send_404(self):
+        self.response.send_response(b'404 Not found', b'text/plain')
+        self.response.send_headers_done()
+        self.response.send_body_data(b'Failed to find ' + self.resource)
+        self.response.send_response_done()
+
+    def handle_request_aborted(self):
+        self.request_aborted = True
+        raise NotImplementedError()
+
+    def handle_data_sent(self, pending_size):
+        raise NotTestedError()
+
+
+def connect_server(port):
+    sock = socket.socket()
+    sock.settimeout(0.1)
+    sock.connect(('127.0.0.1', port))
+    return sock
+
+def parse_response(data):
+    header_end = data.find(b'\r\n\r\n')
+    assert header_end > 0
+    headers = {}
+    for line in data[:header_end].split(b'\r\n'):
+        if not headers:
+            headers[None] = line
+        else:
+            name, value = line.split(b':', 1)
+            assert value.startswith(b' ')
+            value = value[1:]
+            assert name not in headers
+            headers[name] = value
+    body = data[header_end+4:]
+    rest = b''
+    if headers.get(b'Transfer-Encoding') == b'chunked':
+        assert not rest
+        body, rest = parse_chunked_encoding(body)
+    return headers, body, rest
+
+def parse_chunked_encoding(data):
+    body = []
+    done = 0
+    while True:
+        end = data.find(b'\r\n', done)
+        chunklen = int(data[done:end], 16)
+        done = end+2
+        if chunklen == 0:
+            assert data[done:done+2] == b'\r\n'
+            return b''.join(body), data[done+2:]
+        body.append(data[done:done+chunklen])
+        done += chunklen
+        assert data[done:done+2] == b'\r\n'
+        done += 2
+
+class TestBasics(unittest.TestCase):
+    def make_handler(self):
+        handler = Handler()
+        self.handlers.append(handler)
+        return handler
+
+    def setUp(self):
+        self.handlers = []
+        self.server = http_server.HttpServer(
+            handler=self.make_handler, port_low=20000, port_high=21000)
+        self.server_thread = threading.Thread(
+            target=self.server.serve_forever, daemon=True)
+        self.server_thread.start()
+        while self.server.port is None:
+            if not self.server_thread.is_alive():
+                self.fail('Server failed to start')
+            time.sleep(0.0001)
+
+    def test_simple_request(self):
+        sock = connect_server(self.server.port)
+        sock.send(b'GET /simple HTTP/1.1\r\nHost: localhost\r\n\r\n')
+        data = b''
+        while not data.endswith(b'\r\n0\r\n\r\n'):
+            data += sock.recv(4096)
+        self.server.stop()
+        sock.close()
+        self.assertEqual(b'HTTP/1.1 200 OK\r\n', data[:17])
+        self.assertEqual(1, len(self.handlers))
+        handler = self.handlers[0]
+        self.assertEqual({b'Host':b'localhost'}, handler.headers)
+        self.assertEqual(
+            b'\r\n\r\nf\r\nsimple resource\r\n0\r\n\r\n', data[-29:])
+        headers, body, rest = parse_response(data)
+        self.assertEqual(b'', rest)
+        self.assertEqual(b'simple resource', body)
+
+    def test_pipelined_requests(self):
+        sock = connect_server(self.server.port)
+        sock.send(
+            b'GET /simple HTTP/1.1\r\nHost: localhost\r\n\r\n'
+            b'GET /echo/second_request HTTP/1.1\r\nHost: localhost\r\n\r\n')
+        data = b''
+        while not b'second_request' in data:
+            data += sock.recv(4096)
+        sock.send(
+            b'GET /echo/third_request HTTP/1.1\r\nHost: localhost\r\n\r\n')
+        while not b'third_request' in data:
+            data += sock.recv(4096)
+        while not data.endswith(b'\r\n0\r\n\r\n'):
+            data += sock.recv(4096)
+        self.server.stop()
+        sock.close()
+        headers, body, rest = parse_response(data)
+        self.assertEqual(b'simple resource', body)
+        headers, body, rest = parse_response(rest)
+        self.assertEqual(b'echo: second_request', body)
+        headers, body, rest = parse_response(rest)
+        self.assertEqual(b'', rest)
+        self.assertEqual(b'echo: third_request', body)
+
+    def test_parallel_pipelined_requests(self):
+        sock = connect_server(self.server.port)
+        sock2 = connect_server(self.server.port)
+        sock.send(
+            b'GET /simple HTTP/1.1\r\nHost: localhost\r\n\r\n'
+            b'GET /echo/second_request HTTP/1.1\r\nHost: localhost\r\n\r\n')
+        sock2.send(
+            b'GET /echo/parallel_one HTTP/1.1\r\nHost: localhost\r\n\r\n')
+        data = b''
+        data2 = b''
+        while not b'parallel_one' in data2:
+            data2 += sock2.recv(4096)
+        while not b'second_request' in data:
+            data += sock.recv(4096)
+        sock.send(
+            b'GET /echo/third_request HTTP/1.1\r\nHost: localhost\r\n\r\n')
+        sock2.send(
+            b'GET /echo/parallel_two HTTP/1.1\r\nHost: localhost\r\n\r\n')
+        while not b'third_request' in data:
+            data += sock.recv(4096)
+        while not b'parallel_two' in data2:
+            data2 += sock2.recv(4096)
+        while not data.endswith(b'\r\n0\r\n\r\n'):
+            data += sock.recv(4096)
+        while not data2.endswith(b'\r\n0\r\n\r\n'):
+            data2 += sock.recv(4096)
+        self.server.stop()
+        sock.close()
+        sock2.close()
+        headers, body, rest = parse_response(data)
+        self.assertEqual(b'simple resource', body)
+        headers, body, rest = parse_response(rest)
+        self.assertEqual(b'echo: second_request', body)
+        headers, body, rest = parse_response(rest)
+        self.assertEqual(b'', rest)
+        self.assertEqual(b'echo: third_request', body)
+        headers, body, rest = parse_response(data2)
+        self.assertEqual(b'echo: parallel_one', body)
+        headers, body, rest = parse_response(rest)
+        self.assertEqual(b'', rest)
+        self.assertEqual(b'echo: parallel_two', body)
